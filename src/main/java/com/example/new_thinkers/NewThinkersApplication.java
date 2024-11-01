7e3b74ac64d38aba9c7fdfae3ab283338216f597
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-11-01 10:29:10 -0300

    projeto iniciado

---
+++ src/main/java/com/example/new_thinkers/NewThinkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_thinkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewThinkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewThinkersApplication.class, args);
+	}
+
+}
