35d8a2e39b4d99b48a0822bc7a4605f59d782156
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-11-15 14:14:15 -0300

    rota bairro: função criar, listar, atualizar / funcionando

--- src/main/java/com/leandroSS/new_thinkers/controller/BairroController.java
+++ src/main/java/com/leandroSS/new_thinkers/controller/BairroController.java
@@ -2,9 +2,8 @@ package com.leandroSS.new_thinkers.controller;
 
 import com.leandroSS.new_thinkers.dto.ResponseBairroDto;
 import com.leandroSS.new_thinkers.entity.Bairro;
-import com.leandroSS.new_thinkers.entity.UF;
 import com.leandroSS.new_thinkers.service.BairroService;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,36 +12,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/bairro")
 public class BairroController {
+
     private final BairroService bairroService;
 
     public BairroController(BairroService bairroService) {
         this.bairroService = bairroService;
     }
 
-
-    //adicionar pesquisa se UF existe
+    // pp n pegar o codigo municipio do body?
     @PostMapping("/{codigoMunicipio}")
-    public ResponseEntity<ResponseBairroDto> create(@PathVariable("codigoMunicipio") String codigoMunicipio,
-                                                    @RequestBody Bairro bairro) {
-        var newBairro = bairroService.createBairro(codigoMunicipio, bairro);
+    public ResponseEntity<List<ResponseBairroDto>> create(@PathVariable("codigoMunicipio") String codigoMunicipio,
+            @RequestBody Bairro bairro) {
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(newBairro);
+        var newBairro = bairroService.createBairro(codigoMunicipio, bairro);
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(newBairro);
 
     }
 
-    //Rota de pesquisa de Municipio(pesquisar como fazer pesquisa por query em Java)
+    // Rota de pesquisa de Municipio(pesquisar como fazer pesquisa por query em
+    // Java)
     @GetMapping("/")
     public ResponseEntity<List<ResponseBairroDto>> readAll() {
+
         var bairros = this.bairroService.listAllBairro();
-        return ResponseEntity.ok(bairros);
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(bairros);
     }
 
-    //se eu atualizar passando o id esta errado?
-    @PutMapping("/{codigoMunicipio}")
-    public ResponseEntity<List<UF>> Update(@PathVariable("codigoBairro") String codigoBairro,
-                                           @RequestBody Bairro bairro) {
-        this.bairroService.updateBairro(codigoBairro, bairro);
-        return ResponseEntity.noContent().build();
+    // se eu atualizar passando o id esta errado?
+    @PutMapping("/{codigoBairro}")
+    public ResponseEntity<List<ResponseBairroDto>> Update(@PathVariable("codigoBairro") String codigoBairro,
+            @RequestBody Bairro bairro) {
+
+        var updateList = this.bairroService.updateBairro(codigoBairro, bairro);
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(updateList);
     }
 }
-
--- src/main/java/com/leandroSS/new_thinkers/service/BairroService.java
+++ src/main/java/com/leandroSS/new_thinkers/service/BairroService.java
@@ -4,7 +4,7 @@ import com.leandroSS.new_thinkers.dto.ResponseBairroDto;
 import com.leandroSS.new_thinkers.entity.Bairro;
 import com.leandroSS.new_thinkers.repository.BairroRepository;
 import com.leandroSS.new_thinkers.repository.MunicipioRepository;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,68 +12,84 @@ import java.util.List;
 
 @Service
 public class BairroService {
-    private final BairroRepository bairroRepository;
-    private final MunicipioRepository municipioRepository;
 
-    public BairroService(BairroRepository bairroRepository, MunicipioRepository municipioRepository) {
-        this.bairroRepository = bairroRepository;
-        this.municipioRepository = municipioRepository;
-    }
+        private final BairroRepository bairroRepository;
+        private final MunicipioRepository municipioRepository;
 
-
-    public ResponseBairroDto createBairro(String codigoMunicipio, Bairro bairro) {
-
-        var municipio = this.municipioRepository.findById(Integer.valueOf(codigoMunicipio))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Municipio não exixste"));
-
-        var newBairro = new Bairro();
-        newBairro.setStatus(municipio.getStatus());
-        newBairro.setNome(municipio.getNome());
-        newBairro.setMunicipio(municipio);
-
-        this.bairroRepository.save(newBairro);
-
-        return new ResponseBairroDto(
-                newBairro.getCodigoBairro(),
-                newBairro.getMunicipio().getCodigoMunicipio(),
-                newBairro.getNome(),
-                newBairro.getStatus(),
-                null
-        );
-    }
-
-    public List<ResponseBairroDto> listAllBairro() {
-        var allBairro = this.bairroRepository.findAll();
-
-        return allBairro
-                .stream()
-                .map(bairro -> new ResponseBairroDto(
-                        bairro.getCodigoBairro(),
-                        bairro.getMunicipio().getCodigoMunicipio(),
-                        bairro.getNome(),
-                        bairro.getStatus(),
-                        null
-                )).toList();
-    }
-
-    public void updateBairro(String codigoBairro, Bairro bairro) {
-
-        var id = Integer.valueOf(codigoBairro);
-        var BairroEntity = this.bairroRepository.findById(id);
-
-        if (BairroEntity.isPresent()) {
-            var municipioCurrent = BairroEntity.get();
-
-            if (bairro.getNome() != null) {
-                municipioCurrent.setNome(bairro.getNome());
-            }
-
-            if (bairro.getStatus() != null) {
-                municipioCurrent.setStatus(bairro.getStatus());
-            }
-
-            this.bairroRepository.save(municipioCurrent);
+        public BairroService(BairroRepository bairroRepository, MunicipioRepository municipioRepository) {
+                this.bairroRepository = bairroRepository;
+                this.municipioRepository = municipioRepository;
         }
 
-    }
+        public List<ResponseBairroDto> createBairro(String codigoMunicipio, Bairro bairro) {
+
+                var municipio = this.municipioRepository.findById(Integer.valueOf(codigoMunicipio))
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404),
+                                                "Municipio não exixste"));
+
+                var newBairro = new Bairro();
+                newBairro.setStatus(bairro.getStatus());
+                newBairro.setNome(bairro.getNome());
+                newBairro.setMunicipio(municipio);
+
+                this.bairroRepository.save(newBairro);
+
+                var allBairro = this.bairroRepository.findAll();
+
+                return allBairro
+                                .stream()
+                                .map(bairros -> new ResponseBairroDto(
+                                                bairros.getCodigoBairro(),
+                                                bairros.getMunicipio().getCodigoMunicipio(),
+                                                bairros.getNome(),
+                                                bairros.getStatus(),
+                                                null))
+                                .toList();
+        }
+
+        public List<ResponseBairroDto> listAllBairro() {
+                var allBairro = this.bairroRepository.findAll();
+
+                return allBairro
+                                .stream()
+                                .map(bairros -> new ResponseBairroDto(
+                                                bairros.getCodigoBairro(),
+                                                bairros.getMunicipio().getCodigoMunicipio(),
+                                                bairros.getNome(),
+                                                bairros.getStatus(),
+                                                null))
+                                .toList();
+        }
+
+        public List<ResponseBairroDto> updateBairro(String codigoBairro, Bairro bairro) {
+
+                var id = Integer.valueOf(codigoBairro);
+                var bairroCurrent = this.bairroRepository.findById(id)
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404),
+                                                "Bairro não exixste"));
+
+                if (bairro.getNome() != null) {
+                        bairroCurrent.setNome(bairro.getNome());
+                }
+
+                if (bairro.getStatus() != null) {
+                        bairroCurrent.setStatus(bairro.getStatus());
+                }
+
+                this.bairroRepository.save(bairroCurrent);
+
+                var allBairro = this.bairroRepository.findAll();
+
+                return allBairro
+                                .stream()
+                                .map(bairros -> new ResponseBairroDto(
+                                                bairros.getCodigoBairro(),
+                                                bairros.getMunicipio().getCodigoMunicipio(),
+                                                bairros.getNome(),
+                                                bairros.getStatus(),
+                                                null))
+                                .toList();
+
+        }
+
 }
