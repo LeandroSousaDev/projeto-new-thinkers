6fe374b8c1ef1270b6fb5a9e734a8f2bb1aa94f1
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-11-21 17:21:31 -0300

    rota get Municipio feita

--- src/main/java/com/leandroSS/new_thinkers/controller/MunicipioController.java
+++ src/main/java/com/leandroSS/new_thinkers/controller/MunicipioController.java
@@ -3,10 +3,12 @@ package com.leandroSS.new_thinkers.controller;
 import com.leandroSS.new_thinkers.dto.Municipio.CreateMunicipioDto;
 import com.leandroSS.new_thinkers.dto.Municipio.ResponseMunicipioDto;
 import com.leandroSS.new_thinkers.entity.Municipio;
+import com.leandroSS.new_thinkers.entity.UF;
 import com.leandroSS.new_thinkers.service.MunicipioService;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/municipio")
 public class MunicipioController {
     private final MunicipioService municipioService;
+
     public MunicipioController(MunicipioService municipioService) {
         this.municipioService = municipioService;
     }
@@ -27,16 +30,50 @@ public class MunicipioController {
 
     }
 
-    //Rota de pesquisa de Municipio(pesquisar como fazer pesquisa por query em Java)
+    // Rota de pesquisa de Municipio(pesquisar como fazer pesquisa por query em
+    // Java)
     @GetMapping("/")
     public ResponseEntity<List<ResponseMunicipioDto>> readAll() {
         var municipios = this.municipioService.listAllMunicipio();
         return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(municipios);
     }
 
+    @GetMapping("/get/")
+    private ResponseEntity getUf(
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) Integer status,
+            @RequestParam(required = false) Integer codigoMunicipio,
+            @RequestParam(required = false) String uf) {
+
+        ResponseMunicipioDto municipio;
+        List<ResponseMunicipioDto> municipios;
+
+        if (nome != null) {
+            municipios = this.municipioService.municipioByNome(nome);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(municipios);
+
+        } else if (status != null) {
+            municipios = this.municipioService.municipioByStatus(status);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(municipios);
+
+        } else if (codigoMunicipio != null) {
+            municipio = this.municipioService.municipioById(codigoMunicipio);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(municipio);
+
+        } else if (uf != null) {
+            municipios = this.municipioService.municipuioByUF(uf);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(municipios);
+
+        } else {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404), "voce fez uma pesquisa nula");
+
+        }
+
+    }
+
     @PutMapping("/{codigoMunicipio}")
     public ResponseEntity<List<ResponseMunicipioDto>> UpdateUf(@PathVariable("codigoMunicipio") String codigoMunicipio,
-                                                               @RequestBody Municipio municipio) {
+            @RequestBody Municipio municipio) {
 
         var updateList = this.municipioService.updateMunicipio(codigoMunicipio, municipio);
         return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(updateList);
--- src/main/java/com/leandroSS/new_thinkers/repository/MunicipioRepository.java
+++ src/main/java/com/leandroSS/new_thinkers/repository/MunicipioRepository.java
@@ -1,7 +1,20 @@
 package com.leandroSS.new_thinkers.repository;
 
 import com.leandroSS.new_thinkers.entity.Municipio;
+import com.leandroSS.new_thinkers.entity.UF;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MunicipioRepository extends JpaRepository<Municipio, Integer> {
+
+    List<Municipio> findByUf(UF uf);
+
+    List<Municipio> findByNome(String nome);
+
+    List<Municipio> findByStatus(Integer status);
+
+    Municipio findByCodigoMunicipio(Integer codigoMuniciopio);
+
 }
