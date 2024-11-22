cc1ed0577c1341a4a2f320f9af2f8dadb147c019
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-11-22 19:03:53 -0300

    redução de codigo

--- src/main/java/com/leandroSS/new_thinkers/controller/PessoaController.java
+++ src/main/java/com/leandroSS/new_thinkers/controller/PessoaController.java
@@ -1,8 +1,5 @@
 package com.leandroSS.new_thinkers.controller;
 
-import com.leandroSS.new_thinkers.dto.Endereco.CreateEnderecoDto;
-import com.leandroSS.new_thinkers.dto.Endereco.ResponseEnderecoDto;
-import com.leandroSS.new_thinkers.dto.Municipio.ResponseMunicipioDto;
 import com.leandroSS.new_thinkers.dto.Pessoa.CreatePessoaDto;
 import com.leandroSS.new_thinkers.dto.Pessoa.ResponsePessoaDto;
 import com.leandroSS.new_thinkers.service.PessoaService;
@@ -69,29 +66,4 @@ public class PessoaController {
         return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(updatePessoa);
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    @PutMapping("/{codigoEndereco}/endereco")
-    public ResponseEntity<ResponseEnderecoDto> update(@PathVariable("codigoEndereco") String codigoEndereco,
-            @RequestBody CreateEnderecoDto createEnderecoDto) {
-
-        var updateEndereco = this.pessoaService.updateEndereco(codigoEndereco, createEnderecoDto);
-        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(updateEndereco);
-
-    }
-
-    @DeleteMapping("/{codigoEndereco}/endereco")
-    public ResponseEntity<String> delete(@PathVariable("codigoEndereco") String codigoEndereco) {
-        this.pessoaService.deleteEndereco(codigoEndereco);
-        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body("deletado com suceço");
-    }
-
-    @PostMapping("/{codigoPessoa}/endereco")
-    public ResponseEntity<ResponsePessoaDto> create(@PathVariable("codigoPessoa") String codigoPessoa,
-            @RequestBody CreateEnderecoDto createEnderecoDto) {
-
-        var newEndereco = this.pessoaService.addEndereco(codigoPessoa, createEnderecoDto);
-        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(newEndereco);
-
-    }
 }
